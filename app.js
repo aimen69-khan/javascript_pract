b20380ea0de15b418efbd4eff25658549869eba1
Ahmed Raza <aimen69khan@gmail.com>
2026-04-20 11:30:09 +0500

    commit

---
+++ app.js
@@ -0,0 +1,150 @@
+alert(animal);
+alert("Hi World!");
+var b = prompt("enter your age");
+console.log(number + age ('2'));
+
+var userInput;
+userInput
+if(true){
+  console.log(userInput);
+}else{
+  console.log("Hello world!");
+}
+
+if(2===2){
+  if(2===2){
+  console.log(userInput);
+  console.log("My name is Ahmed Raza and I am going to saylani mass it training and I am learning web development.");
+  }else{
+    console.log("Inner If Not Working");
+  }
+  
+}else{
+  console.log("NOT Working");
+}
+
+let userInput = +prompt("enter your number");
+for(var i=1; i<=10; i++){
+  console.log(userInput+" x " +i+ " = " +userInput*i)
+
+
+let age=23;
+age=78;
+
+const name=98;
+
+var a=09;
+var a=98;
+var a=78;
+console.log(a);
+
+
+
+if(2!==2){
+  if(3!==3){
+    if(4!==4){
+      console.log("Hello World!");
+    }else{
+      console.log("not working 4!");
+    }
+  }else{
+    console.log('Not working 3!');
+  }
+}else{
+  console.log("All conditions False working!")
+}
+
+
+var arr = ['Karachi', 'lahore', 'islambad'];
+
+console.log(typeof(arr[arr.length-1]));
+
+if(2===2){
+  var userInput = +prompt("enter your number", 2);
+  console.log(userInput);
+}
+var arr = ['mango', 'banana', 'grape'];
+
+for(var i = 0; i <= 3; i++){
+  console.log(arr);
+  console.log('Hello world!');
+}
+
+
+document.querySelector('body').style.background = "blueviolet";
+
+document.getElementById("demo").style.color = "blue";
+
+
+
+var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+var fullNames = [];
+for (var i = 0; i < firstNames.length; i++) {
+  for (var j = 0; j < lastNames.length; j++) {
+    fullNames.push(firstNames[i] + lastNames[j]);
+  }
+}
+  console.log(fullNames);
+
+
+
+var fruite = ['orange', 'mango', 'grape'];
+var userOutput = prompt("Enter Your Fav fruite");
+var flag = false;
+for(var j=0; j<fruite.length; j++){
+  if(fruite[j] === userOutput){
+    console.log("I Also like " +userOutput +".");
+    flag= true;
+    break;
+  }
+}
+if(!flag){
+  console.log(userOutput+ " :Not found!");
+}
+var arr = ['karachi', 'lahore', 'faisalabad', 'rawalpindi', 'multan', 'islamabad'];
+var found = false;
+var workAfter = false;
+var userInput = prompt("Enter Your City");
+for(var i=0; i<arr.length; i++){
+  if(arr[i] === userInput){
+    console.log("welcome to " +userInput +".");
+    found = true;
+    workAfter = true;
+    if(workAfter){
+      console.log("Found!");
+    }
+    break;
+  }
+}
+if(!found){
+  console.log(userInput +" :Not Found!");
+}
+var country = ['pakistan', 'china', 'japan', 'iran', 'usa'];
+var workFound = false;
+var userOnput = prompt("enter Your country");
+for(var a=0; a<country.length; a++){
+  if(country[a] === userOnput){
+    console.log("Welcome to " +userOnput)
+    workFound = true;
+    break;
+  }
+}
+if(!workFound){
+  console.log(userOnput+ " :Not Found!");
+}
+
+for(var i=1; i<=5; i++){
+  var star = ' ';
+  for(var j=1; j<=i; j++){
+    star+= " *"
+  }
+  console.log(star);
+}
+
+
+var name = "hello WOrLd";
+console.log(name.replaceAll('o', "U"));
+
+var type = "My name is Ahmed";
+
